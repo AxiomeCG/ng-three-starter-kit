415004b93a068dacfdebbe20e09a9e79350348cf
Adam N <Exomus@users.noreply.github.com>
2022-06-27 11:44:28 +0200

    fix: add the envMap settings in the Environment class to apply it to the scene

--- src/app/three-viewer/engine/world/Environment.ts
+++ src/app/three-viewer/engine/world/Environment.ts
@@ -1,7 +1,37 @@
 import { ResourceHandler } from '../handler/resource/ResourceHandler';
 import { DebugGUI } from '../debug/DebugGUI';
-import { DirectionalLight, Scene } from 'three';
+import {
+  DirectionalLight,
+  Material,
+  Mesh,
+  MeshLambertMaterial,
+  MeshPhongMaterial,
+  MeshPhysicalMaterial,
+  MeshStandardMaterial,
+  Scene,
+  sRGBEncoding,
+  Texture
+} from 'three';
 import GUI from 'lil-gui';
+
+/**
+ * Properties of the environment map
+ */
+interface EnvironmentMapHolder {
+  /**
+   * Intensity of the environment map
+   */
+  intensity: number;
+  /**
+   * Cube texture of the environment
+   */
+  texture: Texture;
+
+  /**
+   * Updates the scene to apply the envMap to each material of the scene that supports environment maps.
+   */
+  updateMaterials(): void;
+}
 
 /**
  * Object representing the 3D scene environment properties like lights.
@@ -19,6 +49,12 @@ export class Environment {
   private readonly sunLight: DirectionalLight;
 
   /**
+   * Environment map of the scene to generate the lights on supported materials
+   * @private
+   */
+  private readonly environmentMap: EnvironmentMapHolder;
+
+  /**
    * Constructor
    * @param scene ThreeJS scene to add the lights
    * @param resourceHandler Resources loaded in the project
@@ -33,8 +69,26 @@ export class Environment {
     }
     this.sunLight = new DirectionalLight('#ffffff', 3);
     this.configureSunLight();
+    this.environmentMap = {
+      intensity: 0.4,
+      texture: this.resourceHandler.items.get('environmentMapTexture') as Texture,
+      updateMaterials: () => {
+        this.scene.traverse((child) => {
+          if (child instanceof Mesh && Environment.isEnvMappable(child.material)) {
+            child.material.envMap = this.environmentMap.texture;
+            child.material.envMapIntensity = this.environmentMap.intensity;
+            child.material.needsUpdate = true;
+          }
+        });
+      }
+    };
+
+    this.configureEnvironmentMap();
   }
 
+  /**
+   * Sets up the directional light of the scene (position, shadow casting, shadow optimization ...)
+   */
   configureSunLight(): void {
     this.sunLight.position.set(0.25, 3, -2.25);
     this.sunLight.castShadow = true;
@@ -68,5 +122,40 @@ export class Environment {
     }
   }
 
+  /**
+   * Sets up the environment map of the scene and applies it to the supported materials of the scene.
+   */
+  configureEnvironmentMap() {
+
+    this.environmentMap.texture.encoding = sRGBEncoding;
+
+    this.scene.environment = this.environmentMap.texture;
+
+    this.environmentMap.updateMaterials();
+
+    if (this.debugGUI.ui) {
+      this.debugFolder?.add(this.environmentMap, 'intensity')
+          .name('envMapIntensity')
+          .min(0)
+          .max(4)
+          .step(0.001)
+          .onChange(() => this.environmentMap.updateMaterials());
+    }
+  }
+
+
+  /**
+   * Checks if there are the correct properties on the material for setting the environment map and the environment map
+   * intensity.
+   * @param material Material to check
+   * @private
+   * @returns true if the object possesses the correct attributes to apply the environment map
+   */
+  private static isEnvMappable(material: Material): boolean {
+    return material instanceof MeshStandardMaterial
+      || material instanceof MeshLambertMaterial
+      || material instanceof MeshPhongMaterial
+      || material instanceof MeshPhysicalMaterial;
+  }
 
 }
