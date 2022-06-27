c3dfae32ce5e43379c2a20d731f9a0ca405319bd
Adam N <Exomus@users.noreply.github.com>
2022-06-27 12:55:49 +0200

    docs: update documentation of the TimeHandler

--- src/app/three-viewer/engine/handler/time/TimeHandler.ts
+++ src/app/three-viewer/engine/handler/time/TimeHandler.ts
@@ -13,16 +13,40 @@ export class TimeHandler implements IListenable<void> {
    */
   private readonly eventEmitter = new EventEmitter<void>();
 
+  /**
+   * Start time of the application (at the instantiation of the TimeHandler).
+   * Used to calculate the elapsed time from the start of the experience.
+   * @private
+   */
   private readonly start: number = Date.now();
+  /**
+   * Delta time between the current frame and the old one.
+   * Used to provide the same experience on every framerate.
+   * @private
+   */
   private delta = 0;
+  /**
+   * Current time used to calculate the elapsed time and the delta time.
+   * @private
+   */
   private current = 0;
+  /**
+   * Elapsed time from the start of the experience.
+   * @private
+   */
   private elapsed = 0;
 
+  /**
+   * Constructor
+   */
   constructor() {
     window.requestAnimationFrame(() => this.tick());
     this.tick();
   }
 
+  /**
+   * Handles the time of the experience by emitting on each frame an event to forward updates.
+   */
   tick() {
     const currentTime = Date.now();
     this.delta = currentTime - this.current;
@@ -34,18 +58,35 @@ export class TimeHandler implements IListenable<void> {
     window.requestAnimationFrame(() => this.tick());
   }
 
+  /**
+   * Returns the delta time
+   * @returns The time between the current frame and the previous one.
+   */
   getDeltaTime() {
     return this.delta;
   }
 
+  /**
+   * Returns the current time
+   * @returns A number representing the number of milliseconds for the current frame since January 1, 1970, 00:00:00,
+   *   UTC.
+   */
   getCurrentTime() {
     return this.current;
   }
 
+  /**
+   * Returns the elapsed time from the beginning of the experience.
+   * Commonly used to add time uniform to shaders, for example.
+   * @returns the time elapsed since the application started up
+   */
   getElapsedTime() {
     return this.elapsed;
   }
 
+  /**
+   * Exposes an observable that emits an empty event on the stream to announce a new frame.
+   */
   listen(): Observable<void> {
     return this.eventEmitter.asObservable();
   }
