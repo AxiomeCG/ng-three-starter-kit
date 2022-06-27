ce5f27cb7a52cf81653e72636e08508cfe7a98d5
Adam N <Exomus@users.noreply.github.com>
2022-06-27 12:04:34 +0200

    docs: update documentation on DebugGUI

--- src/app/three-viewer/engine/debug/DebugGUI.ts
+++ src/app/three-viewer/engine/debug/DebugGUI.ts
@@ -1,16 +1,32 @@
 import GUI from 'lil-gui';
 import { IDestroyable } from '../interface/IDestroyable';
 
+/**
+ * Debug tool panel from lil-gui to tweak the project
+ */
 export class DebugGUI implements IDestroyable {
+  /**
+   * lil-gui UI instance
+   */
   readonly ui: GUI | undefined;
+  /**
+   * Status of the GUI based on the URL of the project (add #debug and reload to show the panel)
+   * @private
+   */
   private readonly active = window.location.hash === '#debug';
 
+  /**
+   * Constructor
+   */
   constructor() {
     if (this.active) {
       this.ui = new GUI();
     }
   }
 
+  /**
+   * Destroys the instance of lil-gui UI
+   */
   destroy(): void {
     if (this.active && this.ui) {
       this.ui.destroy();
