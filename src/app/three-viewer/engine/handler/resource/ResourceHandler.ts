399bacfbedc6d81009e61f078e0ef2fb8eba6783
Adam N <Exomus@users.noreply.github.com>
2022-06-27 12:11:49 +0200

    docs: update documentation of the resource handler

--- src/app/three-viewer/engine/handler/resource/ResourceHandler.ts
+++ src/app/three-viewer/engine/handler/resource/ResourceHandler.ts
@@ -5,24 +5,60 @@ import { IListenable } from '../../interface/IListenable';
 import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Source, SourceType } from './Source';
 
+/**
+ * Type of objects that can be loaded into the project
+ */
 export type Item = GLTF | Texture | CubeTexture;
 
+/**
+ * Handles the loading of the project's resources (textures, models...)
+ */
 export class ResourceHandler implements IListenable<void> {
+  /**
+   * Map of the resource items loaded
+   */
   readonly items = new Map<string, Item>();
+  /**
+   * Emits when the loading is done
+   * @private
+   */
   private readonly eventEmitter = new EventEmitter<void>();
+  /**
+   * ThreeJS loading manager
+   * @private
+   */
   private readonly loadingManager = new LoadingManager(() => this.onLoad(),
     (url: string, itemsLoaded: number, itemsTotal: number) => this.onProgress(url, itemsLoaded, itemsTotal),
     (url: string) => this.onError(url)
   );
 
+  /**
+   * Loader for GLTF models
+   * @private
+   */
   private readonly gltfLoader = new GLTFLoader(this.loadingManager);
+  /**
+   * Loader for Textures
+   * @private
+   */
   private readonly textureLoader = new TextureLoader(this.loadingManager);
+  /**
+   * Loader for the Cube Texture for the environment map
+   * @private
+   */
   private readonly cubeTextureLoader = new CubeTextureLoader(this.loadingManager);
 
+  /**
+   * Constructor
+   * @param sourceList List of the source to load
+   */
   constructor(private readonly sourceList: Source[]) {
     this.startLoading();
   }
 
+  /**
+   * Starts the loading of the resource by using the correct loader depending on the type of the resource.
+   */
   startLoading() {
     // Load each source
     for (const source of this.sourceList) {
@@ -57,24 +93,48 @@ export class ResourceHandler implements IListenable<void> {
     }
   }
 
-  sourceLoaded(source: Source, item: Item) {
+  /**
+   * Adds to the map the item loaded
+   * @param source Source of the resource loaded
+   * @param item Item loaded
+   */
+  sourceLoaded(source: Source, item: Item): void {
     this.items.set(source.name, item);
   }
 
+  /**
+   * Returns an observable that notifies the subscribers when the loading is fully completed.
+   */
   listen(): Observable<void> {
     return this.eventEmitter.asObservable();
   }
 
-  private onLoad() {
+  /**
+   * Callback for the loading manager to call when the Loading is completed. It fires the event of the event emitter.
+   * @private
+   */
+  private onLoad(): void {
     console.log('Loading complete!');
     this.eventEmitter.emit();
   }
 
-  private onProgress(url: string, itemsLoaded: number, itemsTotal: number) {
+  /**
+   * Callback for the loading manager to call when an item is loaded.
+   * @param url URL of the object loaded.
+   * @param itemsLoaded Number of items that have been loaded.
+   * @param itemsTotal Total number of items that needs to be loaded to consider the loading done.
+   * @private
+   */
+  private onProgress(url: string, itemsLoaded: number, itemsTotal: number): void {
     console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
   }
 
-  private onError(url: string) {
+  /**
+   * Callback for the loading manager to call when an error occurs when loading an item.
+   * @param url URL of the item that can't be loaded due to an error.
+   * @private
+   */
+  private onError(url: string): void {
     console.log('There was an error loading ' + url);
   }
 }
